f43c6b3dcbc051d4ce020d6ead25f00219b523e5
unknown <sasumro5@gmail.com>
2023-09-05 17:35:52 +0530

    add question linked-list-to-queue-adapter

---
+++ basic_data_structure/linked_list_questions/linked_list_to_queue_adapter.java
@@ -0,0 +1,85 @@
+package linked_list_questions;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class linked_list_to_queue_adapter {
+    public static class LLToQueueAdapter {
+        LinkedList<Integer> list;
+
+        public LLToQueueAdapter() {
+            list = new LinkedList<>();
+        }
+
+        int size() {
+            // write your code here
+            return list.size();
+        }
+
+        void add(int val) {
+            // write your code here
+            list.addLast(val);
+        }
+
+        int remove() {
+            // write your code here
+            if(size() == 0){
+                System.out.println("Stack underflow");
+                return -1;
+            }else{
+                return list.removeFirst();
+            }
+        }
+
+        int peek() {
+            // write your code here
+            if(size() == 0){
+                System.out.println("Stack underflow");
+                return -1;
+            }else{
+                return list.getFirst();
+            }
+        }
+
+        LinkedList<Integer> display() {
+            if(size() == 0){
+                System.out.println("stack underflow");
+                return list;
+            }else{
+                return list;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LLToQueueAdapter qu = new LLToQueueAdapter();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("add")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                qu.add(val);
+            } else if (str.startsWith("remove")) {
+                int val = qu.remove();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("peek")) {
+                int val = qu.peek();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(qu.size());
+            }else if(str.startsWith("display")){
+                LinkedList<Integer> items = qu.display();
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println(items.get(i));
+                }
+            }
+            str = br.readLine();
+        }
+    }
+}
