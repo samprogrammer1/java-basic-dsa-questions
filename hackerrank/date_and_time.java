b80d9a6e154a3d748513ebde89590ef7ab4b3a94
unknown <sasumro5@gmail.com>
2023-10-22 16:50:33 +0530

    get date with day name

---
+++ hackerrank/date_and_time.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import java.util.*;
+
+public class date_and_time {
+    public static void main(String[] args)  {
+        try (Scanner scanner = new Scanner(System.in)) {
+			int month = scanner.nextInt();
+			int day = scanner.nextInt();
+			int year = scanner.nextInt();
+
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(Calendar.DATE, day);
+			calendar.set(Calendar.MONTH, month - 1);
+			calendar.set(Calendar.YEAR, year);
+
+			System.out.println(calendar.getDisplayName(Calendar.DAY_OF_WEEK,
+					Calendar.LONG, new Locale("en", "US")).toUpperCase());
+		}
+    }
+}
